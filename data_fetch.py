52faa008d5647cb687373834f83623453336f2c9
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2022-08-31 13:56:25 +0300

    Create data_fetch.py

---
+++ data_fetch.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import mlrun
+from mlrun import MLClientCtx
+import time
+def data_fetch(context:MLClientCtx):
+    time.sleep(240)
+    df = pd.DataFrame(data={"col":[1,2,3,4]})
+    context.log_dataset(key='df_version_1',df=df,format='csv')
